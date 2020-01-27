356b37a5a1d8d33d963abf15d3f1b8b8eb9dfc05
NWB Admin <paradoxum912@gmail.com>
2020-01-27 22:04:57 +0300

    Сделал skills

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,6 +1,45 @@
 import Vue from "vue";
 
+const  skill = {
+    template: "#skill",
+    props: ["skillName", "skillPercent"],
+    methods: {
+        drawColoredCircle() {
+          const circle = this.$refs["color-circle"];
+          const dashArray = parseInt(
+            getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+          );
+          const percent = (dashArray / 100) * (100 - this.skillPercent);
+    
+          circle.style.strokeDashoffset = percent;
+        }
+      },
+      mounted() {
+        this.drawColoredCircle();
+      }
+}
+
+const  skillsRow = {
+    template: "#skills-row",
+    components: {
+        skill
+    },
+    props: ["row"] //регистрация свойства с именем row
+}
+
 new Vue({
     el: "#skills-component",
-    template: "#skills-list"
-})
+    template: "#skills-list",
+    data() {
+        return {
+            skills: []
+        };
+    },
+    components: {
+        skillsRow
+    },
+    created() {
+        const data = require("../data/skills.json");
+        this.skills = data; //наполнение массива данными из json файла
+    }
+});
