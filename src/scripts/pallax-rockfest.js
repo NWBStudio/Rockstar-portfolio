c081c1155cf96bb258a71470238b544098f0e479
NWB Admin <paradoxum912@gmail.com>
2020-01-29 03:11:28 +0300

    Сделал верхний параллакс

---
+++ src/scripts/pallax-rockfest.js
@@ -0,0 +1,18 @@
+const parallaxRock = document.querySelector('.parallax-rock');
+const parallaxRockLayers = parallaxRock.children;
+
+function moveLayersOnScroll(scroll, layers, speed) {
+    Array.from(layers).forEach(layer => {
+
+        const divider = layer.dataset.speed;
+        const strafe = scroll * divider / speed;
+
+        layer.style.transform = `translateY(-${strafe}%)`;
+    })
+    
+}
+
+window.addEventListener('scroll', e => {
+    const wScroll = window.pageYOffset;
+    moveLayersOnScroll(wScroll, parallaxRockLayers, 14);
+})
