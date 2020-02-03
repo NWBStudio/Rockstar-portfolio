ef390fac66a4c20c31e347779f0dd1e574daf7fa
NWB Admin <paradoxum912@gmail.com>
2020-02-03 20:47:36 +0300

    Небольшие правки валидации

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,33 +1,29 @@
-const   contactsForm = document.querySelector('.contacts__form');
-        newSkillForm = document.querySelector('.works-form');
-
+const contactsForm = document.querySelector('.contacts__form');
 
 keyChecker(contactsForm);
-keyChecker(newSkillForm);
-
 
 function keyChecker(form) {
     form.addEventListener('keydown', e => {
         let target = e.target;
         if (target == form.elements.name) {
-            if(isDigit(e) || isSymbol(e)){
+            if (isDigit(e) || isSymbol(e)) {
                 e.preventDefault();
             }
         }
     });
 }
 
-function isDigit(e){
+function isDigit(e) {
     if (e.key >= 0 || e.key <= 0) return true;
     else return false;
 }
 
-function isControl(e){
+function isControl(e) {
     if (e.key == 'Shift' || e.key == 'Backspace' || e.key == 'ArrowLeft' || e.key == 'ArrowRight') return true;
     else return false;
 }
 
-function isSymbol(e){
+function isSymbol(e) {
     if (e.key == '-' || e.key == '+' || e.key == '/') return true;
     else return false;
 }
