bb2f420df04211000373281f26bfb74821d81998
NWB Admin <paradoxum912@gmail.com>
2020-02-21 17:57:04 +0300

    Тест на jest-puppetier для формы авторизации

--- test/integration/login.test.js
+++ test/integration/login.test.js
@@ -1,32 +1,28 @@
-import faker from "faker";
-import puppeteer from "puppeteer";
+describe('Login page', () => {
+  beforeAll(async () => {
+    await page.goto('http://localhost:8080/admin#/login');
+    await page.waitForSelector(".login-form");
+  })
 
-const APP = "http://localhost:8080/admin#/login";
-
-const fakeUser = {
-  login: faker.internet.userName(),
-  password: faker.internet.password(),
-};
-
-
-let page;
-let browser;
-
-
-beforeAll(async () => {
-  browser = await puppeteer.launch({
-    headless: true,
-    slowMo: 80,
-  });
-  page = await browser.newPage();
-});
-afterAll(() => {
-  browser.close();
-});
-
-describe("Login form", () => {
-  test("Submit btn is exist", async () => {
+  it("Submit button is exist", async () => {
     const submitBtn = await page.$eval(".form__submit-btn", el =>(el ? true : false));
     expect(submitBtn).toBe(true);
   });
-});
+
+  it("Login input field is exist", async () => {
+    const loginInput = await page.$eval("input[name=login]", el =>(el ? true : false));
+    expect(loginInput).toBe(true);
+  });
+  it("Password input field is exist and has correct type", async () => {
+    const passwordInput = await page.$eval("input[type=password]", el =>(el ? true : false));
+    expect(passwordInput).toBe(true);
+  });
+  it("Submit button is disabled until all fields are filled", async () => {
+    let btnIsDisabled = await page.$eval('.form__submit-btn[disabled]', button => button !== null );
+    expect(btnIsDisabled).toBe(true);
+    await page.type("input[name=login]", "lorem");
+    await page.type("input[type=password]", "ipsum");
+    await page.$eval('.form__submit-btn[disabled]', button => button !== null).catch(() => btnIsDisabled = false); 
+    expect(btnIsDisabled).toBe(false);
+  });
+})
